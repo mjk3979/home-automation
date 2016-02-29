4a208ee9705c772c985a7a7c3a91c7a69f132460
Matthew Koontz <mjkoontz93@gmail.com>
2016-02-29 04:29:41 +0000

    Async retry setting input on receiver

--- control.py
+++ control.py
@@ -1,6 +1,7 @@
 import projector_control
 import rxvc.cache as cache
 import time
+import threading
 
 mappings = {
     'desktop':4,
@@ -13,12 +14,19 @@ def get_receiver():
     return receiver
 
 
+def set_input_only(inp):
+    if inp in mappings:
+        get_receiver().input = 'HDMI%d' % (mappings[inp],)
+
 def set_input(inp):
     if inp in mappings:
         projector_control.set_power(True)
         get_receiver().on = True
-        time.sleep(5)
         get_receiver().input = 'HDMI%d' % (mappings[inp],)
+
+        # If the amp is starting, wait 10 seconds and then send the input again
+        threading.Timer(10, lambda: set_input_only(inp)).start()
+        
 
 
 def off():
